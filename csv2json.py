6630361704edb4c995c631440f31ffbe442ced85
Alexandre Dube <adube@mapgears.com>
2013-01-07 09:57:47 -0500

    'explode' into multiple json files using the values of a specified column

--- csv2json.py
+++ csv2json.py
@@ -1,7 +1,9 @@
 #!/usr/local/bin/python
 """
-A simple script for generating JSON/JavaScript from comma-separated (or
-otherwise delimited) values.
+A simple script for generating multiple JSON/JavaScript from comma-separated (or
+otherwise delimited) values. A specified column from the input file must be
+set.  Each record sharing the same field value are put in the same file, so
+one file is produced per unique value for this specified column.
 
 Python 2.7 or higher is recommended see :
 "Floating Point Arithmetic issues and Limitations" at
@@ -10,7 +12,7 @@ http://docs.python.org/2/tutorial/floatingpoint.html
 by Shawn Allen <shawn at stamen dot com>
 modified by Alexandre Dube <adube at mapgears dot com> for lighter json
 """
-import csv
+import csv, sys, os
 try:
     import json
 except ImportError:
@@ -36,7 +38,7 @@ def is_int(s):
     except ValueError:
         return False
 
-def csv2json(csv_file, delimiter=',', quotechar='"', indent=None, callback=None, variable=None, **csv_opts):
+def csv2json(csv_file, directory, column, delimiter=',', quotechar='"', **csv_opts):
     if delimiter_map.has_key(delimiter):
         delimiter = delimiter_map.get(delimiter)
     reader = csv.DictReader(csv_file, delimiter=delimiter, quotechar=quotechar or None, **csv_opts)
@@ -45,7 +47,7 @@ def csv2json(csv_file, delimiter=',', quotechar='"', indent=None, callback=None,
     # csv.DictReader has no mean to return unquoted integer and float values,
     # that's why it's manually done here. None really efficient upon script
     # execution, but json is much lighter that way
-    rows = []
+    columns = {}
     for row in reader:
         for field in row:
             if is_number(row[field]):
@@ -53,21 +55,26 @@ def csv2json(csv_file, delimiter=',', quotechar='"', indent=None, callback=None,
                     row[field] = int(row[field])
                 else:
                     row[field] = float(row[field])
-        rows.append(row)
+        
+        if not row[column] in columns:
+            columns[row[column]] = []
 
-    if hasattr(indent, 'isdigit') and indent.isdigit():
-        indent = ' ' * int(indent)
-    out = StringIO()
-    if callback:
-        out.write('%s(' % callback);
-    elif variable:
-        out.write('var %s = ' % variable)
-    json.dump(rows, out, indent=indent, separators=(',', ':'))
-    if callback:
-        out.write(');');
-    elif variable:
-        out.write(';')
-    return out.getvalue()
+        columns[row[column]].append(row)
+
+
+    # produce 1 json file per unique field value
+    for key in columns:
+        rows = columns[key]
+        outfile = directory+"/"+str(key)+".json"
+        print("Generating: " + outfile)
+
+        out = StringIO()
+        json.dump(rows, out, indent=None, separators=(',', ':'))
+        fo = open(outfile, "wb")
+        fo.write(out.getvalue())
+        fo.close()
+
+    return "done"
 
 if __name__ == '__main__':
     import sys
@@ -87,7 +94,15 @@ if __name__ == '__main__':
                       help='If provided, the output becomes a JavaScript statement'
                       ' which assigns the JSON structure to a variable of the same'
                       ' name.')
+    parser.add_option('-D', '--directory', dest='od', default='.',
+                      help='The destination directory where to save the json files')
+    parser.add_option('-C', '--column', dest='cc', default=None,
+                      help='The column used to explode the records in multiple files')
+
     options, args = parser.parse_args()
+
+    if not os.path.exists(options.od):
+        os.makedirs(options.od)
 
     close = False
     if len(args) > 0 and args[0] != '-':
@@ -95,6 +110,6 @@ if __name__ == '__main__':
         close = True
     else:
         csv_file = sys.stdin
-    print csv2json(csv_file, options.fs, options.fq, options.indent, options.callback, options.var)
+    print csv2json(csv_file, options.od, options.cc, options.fs, options.fq)
     if close:
         csv_file.close()
